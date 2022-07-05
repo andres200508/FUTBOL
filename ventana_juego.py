dcac715ed3ad230082678341bc92589fb1ee2c33
andres200508 <andres.almiron@sagradoalcal.edu.ar>
2022-07-05 16:51:37 -0300

    Reestructuración repositorio

--- ventana_juego.py
+++ ventana_juego.py
@@ -11,26 +11,23 @@ def inicio():
     f1 = tkinter.Frame(ventana_j)
     f2 = tkinter.Frame(ventana_j)
     f3 = tkinter.Frame(ventana_j)
-    
+    '''
     imageUrl = "https://www.escudosfc.com.br/images/rosarioc.png"
     u = urlopen(imageUrl)
     raw_data = u.read()
-    u.close()
+    u.close()'''
     
-    img = ImageTk.Photoimage(data=raw_data)
-    label = tkinter.Label(f1, image = img)
-    label.image = img
-    label.pack()
-    
+    img = ImageTk.PhotoImage(Image.open('Argentina.jpg'))
+    lbl_img = tkinter.Label (f1, image = img)
+    lbl_img.pack()
     nombre = tkinter.Entry(f2, text = "Nombre", bg = "light Blue")
     empezar = tkinter.Button(f3, text = "Confirmar", bg = "white")
-    
+
     f1.grid(column=0, row=0)
     f2.grid(column=0, row=1)
     f3.grid(column=0, row=2)
-    l1.grid(column=0, row=0)
-    nombre.grid()
-    empezar.grid()
+    nombre.grid(row=1, column=0)
+    empezar.grid(row=2, column=0)
     
     
     ventana_j.mainloop()
