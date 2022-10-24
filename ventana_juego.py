29eb7f9e1512ba7a6581cdc0b7c91e3314ed6486
andres200508 <108365341+andres200508@users.noreply.github.com>
2022-10-24 14:52:29 -0300

    azardeequipos

--- ventana_juego.py
+++ ventana_juego.py
@@ -8,21 +8,25 @@ import random
 class VentanaJuego:
     
     def __init__(self):
-        ventana_j = tkinter.Tk()
-        ventana_j.resizable()
-        ventana_j.title("¿Cual Es El Nombre Del Siguiente Equipo?")
+        self.ventana_j = tkinter.Tk()
+        self.ventana_j.resizable()
+        self.ventana_j.title("¿Cual Es El Nombre Del Siguiente Equipo?")
         self.nombre = ""
         self.nombre2 = ""
         self.nombre3 = ""
         self.nombre4 = ""
         self.url = ""
+        self.mostrar_pantalla()
+    
+    def mostrar_pantalla(self):
+        
         self.equiposbd()
-        puntF = tkinter.Frame(ventana_j)
-        escudoF = tkinter.Frame(ventana_j)
-        botonesF = tkinter.Frame(ventana_j)
-        confirmarF = tkinter.Frame(ventana_j)
+        puntF = tkinter.Frame(self.ventana_j)
+        self.escudoF = tkinter.Frame(self.ventana_j)
+        botonesF = tkinter.Frame(self.ventana_j)
+        confirmarF = tkinter.Frame(self.ventana_j)
         puntF.grid(column=0,row=0)
-        escudoF.grid(column=0,row=1)
+        self.escudoF.grid(column=0,row=1)
         botonesF.grid(column=0,row=2)
         confirmarF.grid(column=0,row=3)
         
@@ -30,26 +34,22 @@ class VentanaJuego:
         u = urlopen(self.url)
         raw_data = u.read()
         photo = ImageTk.PhotoImage(data=raw_data)
-        lbl_img = tkinter.Label (escudoF, image = photo)
-        lbl_img.pack()
+        self.lbl_img = tkinter.Label (self.escudoF, image = photo)
+        self.lbl_img.pack()
         
         puntuacion = tkinter.Label(puntF, text = "Puntuacion:", bg = "light blue")
-        confirmar = tkinter.Button(confirmarF, text = "Confirmar", bg = "green4")
         puntuacion.pack()
-        confirmar.pack()
         
-        boton1 = tkinter.Button(botonesF, text=self.nombre, bg="Paleturquoise4", width=35, height=2, command=partial(self.opciones, 0))
-        boton2 = tkinter.Button(botonesF, text =self.nombre2, bg = "Paleturquoise4",  width = 35, height = 2, command=partial(self.opciones, 1))
-        boton3 = tkinter.Button(botonesF, text =self.nombre3, bg = "Paleturquoise4", width = 35, height = 2, command=partial(self.opciones, 2))
-        boton4 = tkinter.Button(botonesF, text =self.nombre4, bg = "Paleturquoise4", width = 35, height = 2, command=partial(self.opciones, 3))
+        self.boton1 = tkinter.Button(botonesF, text=self.nombre, bg="Paleturquoise4", width=35, height=2, command=partial(self.opciones, 0))
+        self.boton2 = tkinter.Button(botonesF, text =self.nombre2, bg = "Paleturquoise4",  width = 35, height = 2, command=partial(self.opciones, 1))
+        self.boton3 = tkinter.Button(botonesF, text =self.nombre3, bg = "Paleturquoise4", width = 35, height = 2, command=partial(self.opciones, 2))
+        self.boton4 = tkinter.Button(botonesF, text =self.nombre4, bg = "Paleturquoise4", width = 35, height = 2, command=partial(self.opciones, 3))
         
-        boton1.grid(column=0,row=0)
-        boton2.grid(column=1,row=0)
-        boton3.grid(column=0,row=1)
-        boton4.grid(column=1,row=1)
-        
-        
-        ventana_j.mainloop()
+        self.boton1.grid(column=0,row=0)
+        self.boton2.grid(column=1,row=0)
+        self.boton3.grid(column=0,row=1)
+        self.boton4.grid(column=1,row=1)
+        self.ventana_j.mainloop()
     
     def equiposbd(self):
         db = sqlite3.connect("futbol_bd")
@@ -57,15 +57,25 @@ class VentanaJuego:
         c.execute('SELECT Nombre,Url from Equipos order by random() limit 4')
         resp = c.fetchall()
         print(resp)
+        self.correcto = random.randint(0,3)
         self.nombre = resp[0][0]
         self.nombre2 = resp[1][0]
         self.nombre3 = resp[2][0]
         self.nombre4 = resp[3][0]
-        self.url = resp[3][1]
+        self.url = resp[self.correcto][1]
+        self.nombres = [self.nombre,self.nombre2,self.nombre3,self.nombre4]
         db.close()
             
     def opciones(self,indice):
-        botones = [boton1, boton2, boton3, boton4]
+        if indice == self.correcto:
+            print("*****************************************************CORRECTO*****************************************************")
+            self.lbl_img.destroy()
+            self.mostrar_pantalla()
+        else:
+            print("❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎ INCORRECTO ❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎")
+            self.ventana_j.destroy()
+            
+            
 
-    
+
        
