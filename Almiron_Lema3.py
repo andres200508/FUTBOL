25227b9a8a966613c1b9f10688c4e58f9e55039a
andres200508 <108365341+andres200508@users.noreply.github.com>
2022-11-28 14:53:54 -0300

    contador (beta)

--- Almiron_Lema3.py
+++ Almiron_Lema3.py
@@ -1,16 +1,13 @@
 import tkinter
 from functools import partial
 from ventana_juego import VentanaJuego
-#boton.config(relief=tkinter.RAISED)
-#boton.config(relief=tkinter.SUNKEN)
-
 
 ventana = tkinter.Tk()
 ventana.resizable(width=0, height=0)
 ventana.title("Test De Equipos del Futbol Argentino")
 
 def elegir_dificultad(boton):
-    botones = [boton1, boton2, boton3, boton4]
+    botones = [boton1, boton2, boton3, boton4, boton5]
     for i in botones:
         i.config(relief=tkinter.RAISED)
     botones[boton].config(relief=tkinter.SUNKEN)
@@ -28,10 +25,11 @@ f3.grid(column=0, row=2)
 
 boton1 = tkinter.Button(f1, text = "Facil", width = 10, height = 5, command=partial(elegir_dificultad, 0), bg="lawn green")
 boton2 = tkinter.Button(f1, text = "Medio", width = 10, height = 5, command=partial(elegir_dificultad, 1), bg="yellow2")
-boton3 = tkinter.Button(f1, text = "Dificil", width = 10, height = 5, command=partial(elegir_dificultad, 2), bg="darkgoldenrod1")
-boton4 = tkinter.Button(f1, text = "Imposible", width = 10, height = 5, command=partial(elegir_dificultad, 3), bg="orangered2")
+boton3 = tkinter.Button(f1, text = "AZAR", width = 10, height = 5, command=partial(elegir_dificultad, 2), bg="magenta")
+boton4 = tkinter.Button(f1, text = "Dificil", width = 10, height = 5, command=partial(elegir_dificultad, 3), bg="darkgoldenrod1")
+boton5 = tkinter.Button(f1, text = "Imposible", width = 10, height = 5, command=partial(elegir_dificultad, 4), bg="orangered2")
 
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 #etiqueta.pack(fill = tkinter.BOTH, expand = True)
 etiqueta = tkinter.Label(f2, text = "Ingrese un nombre de usuario", bg = "cornflower blue")
 nombre = tkinter.Entry(f2, text = "Nombre de usuario", bg = "gray80")
@@ -46,5 +44,5 @@ boton1.grid(row = 1, column = 0)
 boton2.grid(row = 1, column = 1)
 boton3.grid(row = 1, column = 2)
 boton4.grid(row = 1, column = 3)
-
+boton5.grid(row = 1, column = 4)
 ventana.mainloop()    
--- ventana_juego.py
+++ ventana_juego.py
@@ -3,31 +3,33 @@ from PIL import ImageTk, Image
 from functools import partial
 from urllib.request import urlopen
 import sqlite3
-import random
+import string, random
+
+contador = 0
 
 class VentanaJuego:
     
     def __init__(self):
         self.ventana_j = tkinter.Tk()
-        self.ventana_j.resizable()
+        self.ventana_j.resizable(width=False, height=False)
         self.ventana_j.title("¿Cual Es El Nombre Del Siguiente Equipo?")
         self.nombre = ""
         self.nombre2 = ""
         self.nombre3 = ""
         self.nombre4 = ""
         self.url = ""
+        self.L_AZ = ""
         self.mostrar_pantalla()
     
     def mostrar_pantalla(self):
-        
         self.equiposbd()
         puntF = tkinter.Frame(self.ventana_j)
         self.escudoF = tkinter.Frame(self.ventana_j)
         botonesF = tkinter.Frame(self.ventana_j)
         confirmarF = tkinter.Frame(self.ventana_j)
         puntF.grid(column=0,row=0)
-        self.escudoF.grid(column=0,row=1)
-        botonesF.grid(column=0,row=2)
+        self.escudoF.grid(column=0,row=2)
+        botonesF.grid(column=0,row=1)
         confirmarF.grid(column=0,row=3)
         
         
@@ -37,7 +39,7 @@ class VentanaJuego:
         self.lbl_img = tkinter.Label (self.escudoF, image = photo)
         self.lbl_img.pack()
         
-        puntuacion = tkinter.Label(puntF, text = "Puntuacion:", bg = "light blue")
+        puntuacion = tkinter.Label(puntF, text = "Puntuacion:"+ str(contador), bg = "light blue")
         puntuacion.pack()
         
         self.boton1 = tkinter.Button(botonesF, text=self.nombre, bg="Paleturquoise4", width=35, height=2, command=partial(self.opciones, 0))
@@ -50,11 +52,15 @@ class VentanaJuego:
         self.boton3.grid(column=0,row=1)
         self.boton4.grid(column=1,row=1)
         self.ventana_j.mainloop()
-    
+        
+    def sortearletra(self):        
+        self.LAZ = random.choice(string.ascii_letters)
+        
+
     def equiposbd(self):
         db = sqlite3.connect("futbol_bd")
         c = db.cursor()
-        c.execute('SELECT Nombre,Url from Equipos order by random() limit 4')
+        c.execute('SELECT Nombre,Url from Equipos as c where c.Nombre like "%" order by random() limit 4')
         resp = c.fetchall()
         print(resp)
         self.correcto = random.randint(0,3)
@@ -65,17 +71,21 @@ class VentanaJuego:
         self.url = resp[self.correcto][1]
         self.nombres = [self.nombre,self.nombre2,self.nombre3,self.nombre4]
         db.close()
-            
+     
     def opciones(self,indice):
         if indice == self.correcto:
-            print("*****************************************************CORRECTO*****************************************************")
+            print("***************************************************** CORRECTO ***************************************************")
+            global contador
             self.lbl_img.destroy()
             self.mostrar_pantalla()
+            contador = contador + 1
+            print("Puntuacion: " + str(contador) + " equipo(s) acertado(s)")
         else:
-            print("❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎ INCORRECTO ❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎")
+            print("❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎ INCORRECTO ❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎❎")
+            respuesta= self.url
+            print(self.url)
             self.ventana_j.destroy()
+     
             
-            
+'''KÑQWXYZ'''
 
-
-       
